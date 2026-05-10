d0cabef49e011cd73291a4c6ca8390b4663a64e8
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-10 11:54:16 +0400

    Fixed contex

--- cafe/apps.py
+++ cafe/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
 
-
 class CafeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cafe'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@jjcafe.com",
+                password="admin123"
+            )
