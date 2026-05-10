cec822b5aae60400b9ffc558c70e65f77cfe7313
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-10 11:21:05 +0400

    add DB

--- jjcafe/settings.py
+++ jjcafe/settings.py
@@ -84,18 +84,10 @@ WSGI_APPLICATION = 'jjcafe.wsgi.application'
 # ─────────────────────────────────────────
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jjcafe_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # ─────────────────────────────────────────
 # AUTH
 # ─────────────────────────────────────────
