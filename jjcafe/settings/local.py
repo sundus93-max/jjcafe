130f3ffcc103fae4978f9915750526d0cb8eeb01
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-11 10:21:46 +0400

    fix admin proxy model import

--- jjcafe/settings/local.py
+++ jjcafe/settings/local.py
@@ -1,39 +1,29 @@
 from .base import *
-import dj_database_url
-import os
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-#USE_LIVE_DB = os.environ.get('USE_LIVE_DB', 'false').lower() == 'true'
-USE_LIVE_DB = False
-
-if USE_LIVE_DB:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-        )
+# ✅ SQLite for local development
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jjcafe_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'jjcafe_db',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-                'sql_mode': 'STRICT_TRANS_TABLES',
-                'charset': 'utf8mb4',
-            }
-        }
-    }
-
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+SECURE_PROXY_SSL_HEADER = None
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
