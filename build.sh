8037e101ddf1e5d47eac7777ce04a03961b75e3a
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-11 08:30:05 +0400

    fix build script

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+python manage.py makemigrations --no-input
+
+python manage.py migrate --no-input
