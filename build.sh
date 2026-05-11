e8f7f47ab77677b209ecfb02caa533f7e331fee3
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-11 08:37:09 +0400

    fix build - safe superuser creation

--- build.sh
+++ build.sh
@@ -8,3 +8,13 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
+
+# Create superuser only if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'sundus.93@gmail.com', 'jjcafe@2026')
+    print('Superuser created')
+else:
+    print('Superuser already exists - skipped')
+"
