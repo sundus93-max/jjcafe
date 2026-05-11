95d01c38c906200d5d1dccf31ad3013af3504fb7
sundus93@gmail.com <ahamedj935@gmail.com>
2026-05-11 09:25:25 +0400

    update app config / superuser fix

--- cafe/apps.py
+++ cafe/apps.py
@@ -1,6 +1,27 @@
 from django.apps import AppConfig
+import os
+
+#class CafeConfig(AppConfig):
+   # default_auto_field = 'django.db.models.BigAutoField'
+   # name = 'cafe'
 
 
 class CafeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cafe'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
